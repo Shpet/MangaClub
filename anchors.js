4e5e7aceaf9ed7225b512bf5228456b1571892b8
Vadik Shpet <vadik.shpet01@gmail.com>
2019-06-05 16:21:38 +0300

    add anchors and modify <section>

---
+++ anchors.js
@@ -0,0 +1,13 @@
+var scroll_up = document.getElementById('scroll_up');
+scroll_up.onclick = function () {
+    return up();
+};
+
+function up() {
+    var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+    if(top > 0) {
+        window.scrollBy(0,((top+100)/-10));
+        t = setTimeout('up()',20);
+    } else clearTimeout(t);
+         return false;
+}
