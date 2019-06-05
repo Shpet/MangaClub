9ab41941a7106d1a3738ca7c58179dc4b86bc337
Vadik Shpet <vadik.shpet01@gmail.com>
2019-06-05 22:18:39 +0300

    modify "anchors" and start <article>

--- anchors.js
+++ anchors.js
@@ -4,10 +4,17 @@ scroll_up.onclick = function () {
 };
 
 function up() {
-    var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
-    if(top > 0) {
-        window.scrollBy(0,((top+100)/-10));
-        t = setTimeout('up()',20);
+    var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    if (top > 0) {
+        window.scrollBy(0, ((top + 100) / -10));
+        t = setTimeout('up()', 20);
     } else clearTimeout(t);
-         return false;
+    return false;
 }
+
+window.onscroll = function () {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled > 0) {
+        scroll_up.style.display = "block"
+    } else scroll_up.style.display = "none";
+};
